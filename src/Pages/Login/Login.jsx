caab896272a1cef63ca8b116576bd659eab98004
sourov-hossain1 <sourovhossain2122006@gmail.com>
2023-06-07 11:34:18 -0700

    login form added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,34 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login page</h2>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col ">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Please Login!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </div>
+                    <p>You have no account <Link>Register</Link></p>
+                </div>
+                <h2>Google Login</h2>
+            </div>
         </div>
     );
 };
